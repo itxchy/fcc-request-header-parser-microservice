e74b8bb4b162ccc2d893725c37e2e48d48f46e29
Matt Trifilo <matt.trifilo@gmail.com>
2016-08-08 22:56:03 -0500

    add header parsing logic

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const compression = require('compression');
+const parser    = require('ua-parser-js');
 const express     = require('express');
 const app         = express();
 
@@ -7,22 +8,32 @@ app.use(compression());
 
 app.use( (req, res) => {
 
-    let ipAddress;
-    let language;
-    let userAgent;
-
     if (req.url === '/favicon.ico') {
         return;
     }
 
-    ipAddress = req.ip;
-    language = req.get('Accept-Language');
-    userAgent = req.get('User-Agent');
+    let ipAddress = req.ip;
+    let language = req.get('Accept-Language');
+    let userAgent = req.get('User-Agent');
+
+    let reIp = /\d\.?/g;
+    let reLanguage = /,.*/g;
+
+    // returns the matched IP address only
+    let filteredIpAddress = ipAddress.match(reIp).join('');
+
+    // filters out everything after the initial language code
+    let filteredLanguage = language.replace(reLanguage, '');
+
+    // parses User-Agent string into an object
+    let parsedUa = parser(userAgent);
+    let os = parsedUa.os;
+    let filteredOperatingSystem = `${os.name} ${os.version}`;
 
     parsedJSON = {
-        ip_address: ipAddress,
-        language: language,
-        user_agent: userAgent
+        ip_address: filteredIpAddress,
+        language: filteredLanguage,
+        operating_system: filteredOperatingSystem
     };
 
     return res.json(parsedJSON);
